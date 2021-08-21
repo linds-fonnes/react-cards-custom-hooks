1fcf41dd4fa735fd0c0fb350a8b07657736f8f5b
Lindsey <linds.fonnes@gmail.com>
2021-08-20 18:45:26 -0600

    step three useAxios  hook implemented for PlayingCardList;

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const useFlip = () => {
   const [state, setState] = useState(true);
@@ -8,4 +9,22 @@ const useFlip = () => {
   return [state, toggleFlip];
 };
 
-export { useFlip };
+const useAxios = (baseUrl, options = null) => {
+  const [response, setResponse] = useState([]);
+  const [error, setError] = useState(null);
+
+  const fullUrl = !options ? baseUrl : baseUrl + "/" + options;
+  const fetchData = async () => {
+    console.log(fullUrl);
+    try {
+      const resp = await axios.get(fullUrl);
+      setResponse((data) => [...data, resp.data]);
+    } catch (e) {
+      setError(error);
+    }
+  };
+
+  return [response, fetchData];
+};
+
+export { useFlip, useAxios };
